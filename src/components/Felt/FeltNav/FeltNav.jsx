ba3fee5dc4dd418c0bdf64b07bf6d7fcc27bb9e9
msadhimb <adhimbaqy72@gmail.com>
2023-02-11 14:19:43 +0700

    update 1.4

--- src/components/Felt/FeltNav/FeltNav.jsx
+++ src/components/Felt/FeltNav/FeltNav.jsx
@@ -1,24 +1,47 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 function FeltNav() {
+  const [expanded, setExpanded] = useState(false);
   return (
     <Navbar expand="lg">
       <Container className="felt-nav-container">
         <Navbar.Brand href="#home" className="brand">
           <h1>SMF Times</h1>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <i className="fa-solid fa-burger"></i>
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto felt-nav-link">
-            <Nav.Link>
-              <i class="fa-solid fa-magnifying-glass"></i>
+            <Nav.Link className="d-flex justify-content-center align-items-center">
+              <motion.input
+                type="text"
+                initial={{ width: 0, display: "none" }}
+                animate={
+                  expanded ? { display: "block", width: 300 } : { width: 0 }
+                }
+                transition={{ duration: 0.5 }}
+                className="form-control search-input"
+              />
+              <i
+                className="fa-solid fa-magnifying-glass ms-2 me-2"
+                onClick={() => setExpanded(!expanded)}
+              ></i>
             </Nav.Link>
-            <Nav.Link href="#home">
+            <Nav.Link
+              href="#home"
+              className="d-flex justify-content-center align-items-center ms-2 me-2"
+            >
               <i className="fa-brands fa-twitter"></i>
             </Nav.Link>
-            <Nav.Link href="#link">
+            <Nav.Link
+              href="#link"
+              className="d-flex justify-content-center align-items-center ms-2 me-2"
+            >
               <i className="fa-brands fa-instagram"></i>
             </Nav.Link>
           </Nav>
